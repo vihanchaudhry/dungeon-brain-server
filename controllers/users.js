98a69ffbd314c4533fd1f4583bc170c13b4fbf1f
Vihan Chaudhry <vihanchaudhry@gmail.com>
2020-08-16 00:52:08 +0530

    Change password functionality

--- controllers/users.js
+++ controllers/users.js
@@ -2,57 +2,138 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
-exports.register = (req, res) => {
-  bcrypt.hash(req.body.password, 10).then(hash => {
-    const user = new User({
-      email: req.body.email,
-      password: hash,
-      displayName: req.body.displayName,
-    });
+const SALT_ROUNDS = 10;
 
-    user
-      .save()
-      .then(saved => res.status(201).json({ success: true }))
-      .catch(err =>
-        res.status(400).json({ success: false, message: err.message })
-      );
+exports.register = async (req, res) => {
+  // hash password
+  const hash = await bcrypt.hash(req.body.password, SALT_ROUNDS);
+
+  // create new user object
+  const user = new User({
+    email: req.body.email,
+    password: hash,
+    displayName: req.body.displayName,
+  });
+
+  // save user to db
+  const saved = await user
+    .save()
+    .catch(err =>
+      res.status(400).json({ success: false, message: err.message })
+    );
+
+  // success
+  return res.status(201).json({ success: true });
+};
+
+exports.login = async (req, res) => {
+  // find user
+  const user = await User.findOne({ email: req.body.email }).catch(err =>
+    res.status(500).json({ success: false, message: err.message })
+  );
+
+  // if no user by this email in db
+  if (!user) {
+    return res.status(400).json({
+      success: false,
+      message: 'Incorrect email or password.',
+    });
+  }
+
+  // compare password hashes
+  const match = await bcrypt
+    .compare(req.body.password, user.password)
+    .catch(err =>
+      res.status(500).json({ success: false, message: err.message })
+    );
+
+  // if no match, i.e. incorrect password
+  if (!match) {
+    return res.status(400).json({
+      success: false,
+      message: 'Incorrect email or password.',
+    });
+  }
+
+  // auth successful - sign jwt
+  const token = jwt.sign(
+    { _id: user._id, email: user.email, name: user.displayName },
+    process.env.JWT_SECRET_KEY,
+    { expiresIn: '1h' }
+  );
+
+  // return token and user data
+  return res.status(200).json({
+    token,
+    expiresIn: 3600,
+    userId: user._id,
+    displayName: user.displayName,
   });
 };
 
-exports.login = (req, res) => {
-  let user;
-  User.findOne({ email: req.body.email })
-    .then(foundUser => {
-      if (!foundUser) {
-        return res.status(404).json({
-          success: false,
-          message: 'Incorrect email or password.',
-        });
-      }
-      user = foundUser;
-      return bcrypt.compare(req.body.password, user.password);
-    })
-    .then(match => {
-      if (!match) {
-        return res.status(401).json({
-          success: false,
-          message: 'Incorrect email or password.',
-        });
-      }
-      // auth successful - sign jwt
-      const token = jwt.sign(
-        { _id: user._id, email: user.email, name: user.displayName },
-        process.env.JWT_SECRET_KEY,
-        { expiresIn: '1h' }
-      );
-      return res
-        .status(200)
-        .json({
-          token,
-          expiresIn: 3600,
-          userId: user._id,
-          displayName: user.displayName,
-        });
-    })
-    .catch(err => res.status(500).json({ success: false, message: 'Something went wrong.' }));
+exports.changePassword = async (req, res) => {
+  // fetch user
+  const user = await User.findById(req.user._id).catch(err =>
+    res.status(500).json({ success: false, message: err.message })
+  );
+
+  // if user not found
+  if (!user) {
+    return res
+      .status(500)
+      .json({ success: false, message: 'Something went wrong.' });
+  }
+
+  // check if old password is correct
+  const confirmPasswordMatch = await bcrypt
+    .compare(req.body.oldPassword, user.password)
+    .catch(err =>
+      res.status(500).json({ success: false, message: err.message })
+    );
+
+  // if old password not correct
+  if (!confirmPasswordMatch) {
+    return res.status(403).json({
+      success: false,
+      message: 'Incorrect password.',
+    });
+  }
+
+  // check if new password is the same as old password
+  const sameNewPassword = await bcrypt
+    .compare(req.body.newPassword, user.password)
+    .catch(err =>
+      res.status(500).json({ success: false, message: err.message })
+    );
+
+  // if new password and old password are the same
+  if (sameNewPassword) {
+    return res.status(400).json({
+      success: false,
+      message: 'The new password cannot be the same as your old password.',
+    });
+  }
+
+  // hash new password
+  const hash = await bcrypt
+    .hash(req.body.newPassword, SALT_ROUNDS)
+    .catch(err =>
+      res.status(500).json({ success: false, message: err.message })
+    );
+
+  // set user password to new hashed value
+  user.password = hash;
+
+  // save user document
+  const saved = await user
+    .save()
+    .catch(err =>
+      res.status(500).json({ success: false, message: err.message })
+    );
+
+  // return success
+  res.status(200).json({
+    success: true,
+    message: 'Your password has been changed.',
+  });
 };
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const UsersController = require('../../controllers/users');
+const authorization = require('../../middleware/authorization');
 
 const router = express.Router();
 
@@ -10,5 +11,9 @@ router.post('/register', UsersController.register);
 // @desc    Login user
 // @route   POST /api/users/login
 router.post('/login', UsersController.login);
+
+// @desc    Change user password
+// @route   POST /api/users/change-password
+router.post('/change-password', authorization, UsersController.changePassword);
 
 module.exports = router;
